b73077689c3e9e2048ed167c346d047782cad747
Ian King <ian.antking@gmail.com>
2019-01-23 17:15:04 +0000

    api serves index.html

--- src/app.js
+++ src/app.js
@@ -5,7 +5,11 @@ const ImageRouter = require('./routes/images');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('images'));
 
 app.use('/images', ImageRouter);
+app.get('*', (_, res) => {
+  res.redirect('/');
+});
 
 module.exports = app;
